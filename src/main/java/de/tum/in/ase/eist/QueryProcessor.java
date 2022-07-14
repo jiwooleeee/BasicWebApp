1bb369b396f1ac84bcd58259c6ec4ecd900cf8c0
jiwoo <jiwoo.lee@web.de>
2022-07-14 17:17:14 +0200

    query

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -20,6 +20,8 @@ public class QueryProcessor {
             return "=14";
         } else if (query.contains("8")){
             return "8";
+        } else if (query.contains("heroku")){
+            return "Heroku is a cloud platform as a service supporting several programming languages. One of the first cloud platforms, Heroku has been in development since June 2007, when it supported only the Ruby programming language, but now supports Java, Node.js, Scala, Clojure, Python, PHP, and Go.";
         } else {
             return "";
         }
